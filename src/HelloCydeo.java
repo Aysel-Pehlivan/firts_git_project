9effee3252dd9289cc306a21580b6e179edd7c6f
Aysel <ayselarmy@hotmail.com>
2024-12-11 16:14:19 -0500

    Update HelloCydeo.java

--- src/HelloCydeo.java
+++ src/HelloCydeo.java
@@ -1,5 +1,11 @@
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+         System.out.println("Hello Word");
+         System.out.println("Hello Programer");
+       
     }
-}
+    }
+    
+    
+
